bf515b2d6ed0c736c83691e830e8ee55b18e0367
Loveayu <jiadesen1993@163.com>
2016-12-03 03:11:14 +0800

    UI-China
    添加分页功能

--- UI-China/data/ui_china.php
+++ UI-China/data/ui_china.php
@@ -1,26 +1,52 @@
 <?php
-    header('Content-Type:application/json;charset=UTF-8');
-    //服务器信息
-    $db_url = '127.0.0.1';
-    $db_user = 'root';
-    $db_pwd = '';
-    $db_name = 'ui_china';
-    $db_port = '3306';
-    //新浪云
-    //$db_url = SAE_MYSQL_HOST_M;
-    //$db_user = SAE_MYSQL_USER;
-    //$db_pwd = SAE_MYSQL_PASS;
-    //$db_name = SAE_MYSQL_DB;
-    //$db_port = SAE_MYSQL_PORT;
-    //接收客户端提交的类别
-    $type = $_REQUEST['type'];
-    $conn = mysqli_connect($db_url,$db_user, $db_pwd, $db_name, $db_port);
-    $sql = "SET NAMES UTF8";
-    mysqli_query($conn,$sql);
-    $arr = [];
-    $sql = "SELECT * FROM $type ORDER BY hid LIMIT 20";
-    $result = mysqli_query($conn,$sql);
-    while(($list = mysqli_fetch_assoc($result))!=null){
-        $arr[] = $list;
-    }
-    echo json_encode($arr);
+header('Content-Type:application/json;charset=UTF-8');
+//服务器信息
+$db_url = '127.0.0.1';
+$db_user = 'root';
+$db_pwd = '';
+$db_name = 'ui_china';
+$db_port = '3306';
+//新浪云
+//$db_url = SAE_MYSQL_HOST_M;
+//$db_user = SAE_MYSQL_USER;
+//$db_pwd = SAE_MYSQL_PASS;
+//$db_name = SAE_MYSQL_DB;
+//$db_port = SAE_MYSQL_PORT;
+
+//接收客户端提交的类别
+$type = $_REQUEST['type'];
+$pageNum = $_REQUEST['pageNum'];
+
+/***向客户端输出了分页对象***/
+$pager = [
+    'type' => $type,
+    'recorderCount' => 0, //总记录数
+    'pageSize' => 20, //页面大小
+    'pageCount' => 0, //总页数
+    'pageNum' => intval($pageNum), //当前页号
+    'data' => null
+];
+/**************************/
+
+//连接数据库
+$conn = mysqli_connect($db_url, $db_user, $db_pwd, $db_name, $db_port);
+//设置编码方式
+$sql = "SET NAMES UTF8";
+mysqli_query($conn, $sql);
+//获取总记录数，计算总页数
+$sql = "SELECT COUNT(*) FROM $type";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$pager['recorderCount'] = intval($row['COUNT(*)']); //将字符串解析为整数
+$pager['pageCount'] = ceil(($pager['recorderCount']) / ($pager['pageSize']));
+
+//获取当前指定页中的记录
+$start = ($pager['pageNum'] - 1) * $pager['pageSize']; //从哪一行开始获取数据
+$count = $pager['pageSize']; //读取多上行
+$sql = "SELECT * FROM $type LIMIT $start,$count";
+$result = mysqli_query($conn, $sql);
+
+//读取所有数据记录
+$pager['data'] = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//输出
+echo json_encode($pager);
--- UI-China/js/index.js
+++ UI-China/js/index.js
@@ -1,16 +1,20 @@
 //功能点1：页面内容异步加载
 //封装ajax，根据所需分类，异步加载内容
-function getAjax(type) {
+function loadDataByTypeAndPage(type, pageNum) {
     $.ajax({
         url: 'data/ui_china.php',
-        data: {'type': type},
-        success: function (list, msg, xhr) {
+        data: {
+            'type': type,
+            'pageNum': pageNum
+        },
+        success: function (pager, msg, xhr) {
             // console.log('开始处理响应数据');
-            console.log(list);
+            // console.log(pager);
             // console.log(msg);
             // console.log(xhr);
             var html = '';
-            $.each(list, function (i, arr) {
+            $.each(pager.data, function (i, arr) {
+                // console.log(arr);
                 html += `
                       <dl>
                         <dt><img src="${arr.rec_img}"/></dt>
@@ -21,24 +25,73 @@ function getAjax(type) {
                       `;
             });
             $('.main').html(html);
+            //根据返回的分页数据，动态创建分页条内容
+            var html_page = '';
+
+            // if (pager.pageNum - 2 > 0) {
+            //     html_page += `<li><a href="${pager.type}" rel="${pager.pageNum - 2}">${pager.pageNum - 2}</a></li>`;
+            // }
+            // if (pager.pageNum - 1 > 0) {
+            //     html_page += `<li><a href="${pager.type}" rel="${pager.pageNum - 1}">${pager.pageNum - 1}</a></li>`;
+            // }
+            // html_page += `<li class="active"><a href="${pager.type}" rel="${pager.pageNum}">${pager.pageNum}</a></li>`;
+            // if (pager.pageNum + 1 > 0) {
+            //     html_page += `<li><a href="${pager.type}" rel="${pager.pageNum + 1}">${pager.pageNum + 1}</a></li>`;
+            // }
+            // if (pager.pageNum + 2 > 0) {
+            //     html_page += `<li><a href="${pager.type}" rel="${pager.pageNum + 2}">${pager.pageNum + 2}</a></li>`;
+            // }
+
+            var str = `<li>共${pager.pageCount}页/当前是第${pager.pageNum}页</li>`;
+            var first = (pager.pageNum == 1) ? `<li>首页</li>` : `<li><a href="${pager.type}" rel="1">首页</a></li>`;
+            var last = (pager.pageNum == pager.pageCount) ? `<li>尾页</li>` : `<li><a href="${pager.type}" rel="${pager.pageCount}">尾页</a></li>`;
+            var prev = (pager.pageNum == 1) ? `<li>上一页</li>` : `<li><a href="${pager.type}" rel="${pager.pageNum - 1}">上一页</a></li>`;
+            var next = (pager.pageNum == pager.pageCount) ? `<li>下一页</li>` : `<li><a href="${pager.type}" rel="${pager.pageNum + 1}">下一页</a></li>`;
+            for (var i = 1, p = ''; i <= pager.pageCount; i++) {
+                if (i == 1) {
+                    p = `<li class="p"><a class="p" href="${pager.type}" rel="${i}">${i}</a></li>`;
+                } else {
+                    p += `<li class="p"><a class="p" href="${pager.type}" rel="${i}">${i}</a></li>`;
+                }
+            }
+            html_page = first + prev + p + next + last + str;
+            $('.pager').html(html_page);
         }
     })
 }
+
 //功能点2：页面第一次加载时的默认显示
 window.onload = function () {
     var type = 'homepage';
-    getAjax(type);
+    var pageNum = '1';
+    loadDataByTypeAndPage(type, pageNum);
 };
-//功能点3：鼠标点击时异步请求对应的数据列表
+
+//功能点3：鼠标点击分类时异步请求对应的数据列表
 $('.column ul li').on("click", "a", function (e) {
     e.preventDefault();
     $(this).parent().addClass('active').siblings('.active').removeClass('active');
-//  异步请求当前类别下的列表
+    //异步请求当前类别下的列表
     var type = $(this).attr('href');
     // console.log(type);
-    getAjax(type);
+    var pageNum = $(this).attr('rel');
+    // console.log(pageNum);
+    loadDataByTypeAndPage(type, pageNum);
 });
-//功能点4：轮播
+
+//功能点4：鼠标点击页码时异步请求对应的数据
+$('.page').on("click", "a", function (e) {
+    e.preventDefault();
+    // $(this).parent().addClass('active').siblings('.active').removeClass('active');
+    //异步请求当前类别下对应页码的列表
+    var type = $(this).attr('href');
+    // console.log(type);
+    var pageNum = $(this).attr('rel');
+    // console.log(pageNum);
+    loadDataByTypeAndPage(type, pageNum);
+});
+
+//功能点5：轮播
 var t = n = 0, count;
 $(document).ready(function () {
     count = $("#banner_list a").length;
@@ -49,7 +102,7 @@ $(document).ready(function () {
         if (i >= count) return;
         //遍历当前可见的图片使其淡出
         $("#banner_list a").filter(":visible").fadeOut(500)
-            //找到选中的图片使其淡入
+        //找到选中的图片使其淡入
             .parent().children().eq(i).fadeIn(1000);
         $(this).toggleClass("on");
         $(this).siblings().removeAttr("class");
@@ -61,12 +114,12 @@ $(document).ready(function () {
         t = setInterval("showAuto()", 3000);
     })
 });
-
 function showAuto() {
     n = n >= (count - 1) ? 0 : ++n;
     $("#banner li").eq(n).trigger('click');
 }
-//功能点5：返回页面顶部
+
+//功能点6：返回页面顶部
 $(function () {
     $(window).scroll(function () {  //只要窗口滚动,就触发下面代码
         var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
